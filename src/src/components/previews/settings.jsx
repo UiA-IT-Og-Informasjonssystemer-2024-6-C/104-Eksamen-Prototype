7c33686aead8e86a5a6464a3735353b200cf2e2c
Joachim Flottorp <joachim@flottorp.org>
2024-11-05 13:09:31 +0100

    Settings side (#4)

--- src/src/components/previews/settings.jsx
+++ src/src/components/previews/settings.jsx
@@ -1,3 +1,52 @@
+import AppBar from "../app-bar";
+import NavigationBar from "../navigation-bar";
+import "./settings.css";
+
 export default function Settings() {
-	return <div>Settings</div>;
+	return (
+		<div>
+			<NavigationBar />
+
+			<div class="container">
+				<div class="title">Innstillinger</div>
+
+				<div class="menu-item">
+					<i class="icon-settings"></i>
+					<span>Allergen</span>
+				</div>
+
+				<div class="menu-item">
+					<i class="icon-globe"></i>
+					<span>Språk</span>
+				</div>
+
+				<div class="menu-item">
+					<i class="icon-bell"></i>
+					<span>Varsler</span>
+				</div>
+
+				<div class="menu-item">
+					<i class="icon-favorites"></i>
+					<span>Favoritter</span>
+				</div>
+
+				<div class="menu-item">
+					<i class="icon-user"></i>
+					<span>Konto</span>
+				</div>
+
+				<div class="menu-item">
+					<i class="icon-lock"></i>
+					<span>Sikkerhet</span>
+				</div>
+
+				<div class="menu-item">
+					<i class="icon-logout"></i>
+					<span>Bytt konto/Log av</span>
+				</div>
+			</div>
+
+			<AppBar />
+		</div>
+	);
 }
