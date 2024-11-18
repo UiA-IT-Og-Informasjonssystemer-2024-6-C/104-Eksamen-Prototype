3854ae2f4c9776705334e126b7e61fcaf4df1562
Joachim Flottorp <joachim@flottorp.org>
2024-11-18 10:38:38 +0100

    Gjør slett knappen om til en søppelkasse (#15)

--- src/src/components/button.jsx
+++ src/src/components/button.jsx
@@ -1,9 +1,10 @@
 export default function Button(props) {
-	const { text } = props;
+	const { text, icon, ...restProps } = props;
 
 	return (
-		<button class="button" {...props}>
-			{text}
+		<button class="button" {...restProps}>
+			{icon && <span className="icon">{<img src={icon} />}</span>}
+			{text && <span className="text">{text}</span>}
 		</button>
 	);
 }
